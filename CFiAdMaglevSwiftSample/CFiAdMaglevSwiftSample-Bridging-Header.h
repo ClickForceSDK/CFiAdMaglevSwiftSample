814ad62e6a1115c729ece2d53cd880919ef5427b
Eric <>
2019-08-08 14:13:27 +0800

    ClickForce IOS AdMaglev Swift Sample Project Update

---
+++ CFiAdMaglevSwiftSample/CFiAdMaglevSwiftSample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  CFiAdMaglevSwiftSample-Bridging-Header.h
+//  CFiAdMaglevSwiftSample
+//
+//  Created by CF-NB on 2019/8/7.
+//  Copyright © 2019年 CF-NB. All rights reserved.
+//
+
+#ifndef CFiAdMaglevSwiftSample_Bridging_Header_h
+#define CFiAdMaglevSwiftSample_Bridging_Header_h
+
+
+#endif /* CFiAdMaglevSwiftSample_Bridging_Header_h */
+#import <iMFAD/iMFAD.h> 
---
+++ CFiAdMaglevSwiftSample/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  CFiAdMaglevSwiftSample
+//
+//  Created by CF-NB on 2019/8/7.
+//  Copyright © 2019年 CF-NB. All rights reserved.
+//
+
+import UIKit
+import iMFAD
+class ViewController: UIViewController {
+    
+    var admaglev : MFAdMaglev?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+        admaglev = MFAdMaglev.init();
+        admaglev?.bannerId = "9800";
+        admaglev?.delegate = self;
+        admaglev?.debugInfo(true);
+        admaglev?.requestAd();
+    }
+}
+
+extension ViewController:MFAdMaglevDelegate{
+    func onSuccessToMFAdMaglev() {
+        print("onSuccessToMFAdMaglev");
+        admaglev?.show();
+    }
+    
+    func onFailToMFAdMaglev() {
+        print("onFailToMFAdMaglev");
+    }
+    
+    func onClickToMFAdMaglev() {
+        print("onClickToMFAdMaglev");
+    }
+    
+    func onCloseToMFAdMaglev() {
+        print("onCloseToMFAdMaglev");
+    }
+}
+
